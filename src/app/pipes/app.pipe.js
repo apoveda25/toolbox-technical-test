faef70ab25eb31c9c429697ab2856504b54b3d44
Alfredo Poveda <alfpovsistemas@gmail.com>
2022-01-03 16:55:03 -0400

    feature(Create endpoint GET /iecho)

---
+++ src/app/pipes/app.pipe.js
@@ -0,0 +1,13 @@
+const appPipe = (req, res, next) => {
+    const { text } = req.query;
+
+    if (!text) res.status(400).json({
+        "error": "no text"
+    })
+
+    req.appPipeResult = { text };
+
+    next()
+}
+
+module.exports = { appPipe }
